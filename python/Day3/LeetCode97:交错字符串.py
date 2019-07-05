9a5cdb975ec5acd573e1bb16fe10c8d81160a293
Janice18 <49306576+Janice18@users.noreply.github.com>
2019-07-05 11:36:52 +0800

    Update LeetCode97:交错字符串.py

--- python/Day3/LeetCode97:交错字符串.py
+++ python/Day3/LeetCode97:交错字符串.py
@@ -1,5 +1,5 @@
 """
-方法：动态规划（动态方程），dim[i][j]表示s1的前(i+1)元素和s2的前(j+1)元素是否交错组成s3前i+j元素
+方法1：动态规划（动态方程），dim[i][j]表示s1的前(i+1)元素和s2的前(j+1)元素是否交错组成s3前i+j元素
 主要考虑四种情况：
 1.i=0 & j≠0，则返回dim[i][j] = dim[i][j-1] && s2[j-1] == s3[i+j-1]
 2.i≠0 & j=0，则返回dim[i][j] = dim[i-1][j] && s1[i-1] == s3[i+j-1]
@@ -44,6 +44,29 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
+ 
+"""
+法二：递归，思路如下：
+先检查s3的第一个字符，如果等于s1的第一个字符，且不等于s2的第二个字符，将s1和s3向后移动一位，递归。
+当等于s2的第一个字符且不等于与s1的第一个字符的时候，将s2和s3向后移动一位，递归。
+如果既等于s1的第一个字符又等于s2的第一个字符，
+就有可能有两种情况，s2不动，s1向后移动一位或者是s1不动，s2向后移动一位。
+"""
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s3) != len(s1) + len(s2):
+            return False
+        if s1 == "" and s2 == "" and s3 == "":
+            return True
+        if s1 == "":
+            return s2 == s3
+        if s2 == "":
+            return s1 == s3
+        if s3[0] == s1[0] and s3[0] != s2[0]:
+            return self.isInterleave(s1[1:],s2,s3[1:])
+        elif s3[0] == s2[0] and s3[0] != s1[0]:
+            return self.isInterleave(s1,s2[1:],s3[1:])
+        elif s3[0] == s2[0] and s3[0] == s1[0]:
+            return self.isInterleave(s1[1:],s2,s3[1:]) or self.isInterleave(s1,s2[1:],s3[1:])
+        else:
+            return False
