93c924584434d4dd334b43e6875404b05a1c67e8
Janice18 <49306576+Janice18@users.noreply.github.com>
2019-07-05 11:58:13 +0800

    Update LeetCode131:分割回文串II.py

--- python/Day5(Quizs)/LeetCode131:分割回文串II.py
+++ python/Day5(Quizs)/LeetCode131:分割回文串II.py
@@ -1,9 +1,27 @@
 """
-方法:动态规划
-可以通过动态规划解决，dp[i]表示字符串s[:i+1]需要的最少的切割次数，dp[i]的初始值为i，因为长度为i+1的字符串最多切割i次就能满足题目要求 。 
- 当添加一个字符后，我们需要依次判断以它为末尾的子字符串是否是回文字符串，
- 如果是，则要计算剩余字符串需要的最少切割次数加上一次是否能使当前的最少切割次数更少. 
+方法：动态规划
+思路：创建一组arr来存储每个位置的初始最小分割次数，其中索引为0的元素指为-1，
+在每个存储位置判断是否可以分割，再不断去更新原来arr的值，最后返回arr[-1]即可
 """
+class Solution:
+    def minCut(self, s: str) -> int:
+        """
+     :type s: str
+     :rtype: int
+     """
+
+        arr = [len(s) for i in range(len(s)+1)]
+        arr[0] = -1
+        for i in range(len(s)):
+            for j in range(i + 1):
+                if s[j:i + 1] == s[j:i + 1][::-1]:
+                    arr[i + 1] = min(arr[i + 1], arr[j] + 1)
+        return arr[-1]
+
+if __name__ == '__main__':
+    print(Solution().minCut('aab'))
+
+#表示2
 class Solution(object):
     def minCut(self, s: str) -> int:
         n = len(s)
